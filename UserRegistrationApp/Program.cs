d4aa8b1f4d1fd4a0ed9649b93f1ae6c5ed390e29
Wpaswan <dablumcapaswan@gmail.com>
2021-12-14 08:51:00 -0800

    [I,added a code to validate First name]

---
+++ UserRegistrationApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using UserRegistrationApp;
+Console.WriteLine("Enter user name:");
+string userName = Console.ReadLine();
+Registration UName = new Registration();
+if (UName.ValidateUserName(userName)==true)
+    Console.WriteLine("User name accepted");
+else
+    Console.WriteLine("Invalid!!First letter must be capital and minimum 3 characters requiered!!");
---
+++ UserRegistrationApp/Registration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationApp
+{
+    internal class Registration
+    {
+        public static string FirsName = "^[A-Z]{1}[a-zA-Z0-9]{2,29}$";
+        public bool ValidateUserName(string uName)
+        {
+            return Regex.IsMatch(uName, FirsName);
+        }
+    }
+}
