b317fce4bfd9f933c3a69eb316e0492fc5751b2a
Wpaswan <dablumcapaswan@gmail.com>
2021-12-14 08:57:18 -0800

    [I added a code to validate Last name]

--- UserRegistrationApp/Program.cs
+++ UserRegistrationApp/Program.cs
@@ -6,3 +6,10 @@ if (UName.ValidateUserName(userName)==true)
     Console.WriteLine("User name accepted");
 else
     Console.WriteLine("Invalid!!First letter must be capital and minimum 3 characters requiered!!");
+Console.WriteLine("Enter Last Name:");
+string LastName = Console.ReadLine();
+Registration LName = new Registration();
+if (LName.ValidateLasttName(LastName)==true)
+    Console.WriteLine("Last name accepted");
+else
+    Console.WriteLine("Invalid!!Last letter must be capital and minimum 3 characters requiered!!");
--- UserRegistrationApp/Registration.cs
+++ UserRegistrationApp/Registration.cs
@@ -10,7 +10,12 @@ namespace UserRegistrationApp
     internal class Registration
     {
         public static string FirsName = "^[A-Z]{1}[a-zA-Z0-9]{2,29}$";
+        public static string LastName = "^[A-Z]{1}[a-zA-Z0-9]{2,29}$";
         public bool ValidateUserName(string uName)
+        {
+            return Regex.IsMatch(uName, FirsName);
+        }
+        public bool ValidateLasttName(string uName)
         {
             return Regex.IsMatch(uName, FirsName);
         }
